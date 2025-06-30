eddf4c28baf9982833e600a64e1da238dab5c550
Roman Šimunović <67165057+romanuspopulsque@users.noreply.github.com>
2025-06-30 17:51:55 +0200

    Update Radionice.js

--- src/pages/Radionice.js
+++ src/pages/Radionice.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
-const API = process.env.REACT_APP_API_BASE_URL;
+const baseUrl = "https://eduplusbackend.onrender.com";
 
 function Radionice() {
   const [radionice, setRadionice] = useState([]);
   const [naziv, setNaziv] = useState('');
 
   useEffect(() => {
-    fetch(`${API}/api/radionice`)
+    fetch(`${baseUrl}/api/radionice`)
       .then(res => res.json())
       .then(data => setRadionice(data));
   }, []);
 
   const handleAdd = () => {
-    fetch(`${API}/api/radionice`, {
+    fetch(`${baseUrl}/api/radionice`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ naziv })
