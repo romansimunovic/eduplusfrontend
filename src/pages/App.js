4e18c0907be749c767ba253c509a3dbc057ac7d4
Roman Šimunović <67165057+romanuspopulsque@users.noreply.github.com>
2025-07-01 13:30:41 +0200

    Create App.js

---
+++ src/pages/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Radionice from './pages/Radionice';
+import Polaznici from './pages/Polaznici';
+import Prisustva from './pages/Prisustva';
+import './App.css'; // obavezno da CSS stilovi rade
+
+function App() {
+  return (
+    <Router>
+      <div className="header-banner">
+        <h1>EdukatorPlus</h1>
+        <p>Digitalna platforma za evidenciju edukacija, polaznika i prisustva</p>
+      </div>
+
+      <nav>
+        <ul className="nav-links">
+          <li><Link to="/">Radionice</Link></li>
+          <li><Link to="/polaznici">Polaznici</Link></li>
+          <li><Link to="/prisustva">Prisustva</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Radionice />} />
+        <Route path="/polaznici" element={<Polaznici />} />
+        <Route path="/prisustva" element={<Prisustva />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
