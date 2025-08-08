bef75730b0c37e0d173bfd2fa0fc8f7e8f8b2a7d
Roman Šimunović <67165057+romansimunovic@users.noreply.github.com>
2025-08-08 11:40:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom'; // <-- ključna stvar
+import { HashRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <App />
