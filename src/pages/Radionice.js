8201227b47c59b8f7e2eadeaa786bd2487e55f13
Roman Šimunović <67165057+romansimunovic@users.noreply.github.com>
2025-08-08 10:36:18 +0200

    Update Radionice.js

--- src/pages/Radionice.js
+++ src/pages/Radionice.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
-import { api } from './api';
+import { api } from '../api';
 
 function Radionice() {
   const [radionice, setRadionice] = useState([]);
