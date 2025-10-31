5260093a9eb3e22d7e5da2f18ac78002d16c331e
Roman Šimunović <romansimunovic21@gmail.com>
2025-10-31 23:41:07 +0100

    Izmjena index

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
-import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
